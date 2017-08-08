10456bedac0437b880c7a4d97ad5d032ddfce16c
Prappers <30434976+Prappers@users.noreply.github.com>
2017-08-08 23:11:40 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'weer',
+		wp_ws_url : 'http://www.trumpnieuws.com/wp-appkit-api/weer',
+		wp_url : 'http://www.trumpnieuws.com',
+		theme : 'q-android',
+		version : '1.0.0',
+		app_title : 'Weer',
+		app_platform : 'android',
+		gmt_offset : 2,
+		debug_mode : 'off',
+		auth_key : 'pw9LRXnsts@Uzrs1DyuY$Sh$v7Eq^L3J#bRv@I$$%fMf)#hI4@hAMdlUh@urHnEm',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
